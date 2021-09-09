8746291f13a3ecf2dd38a2751a7a7c3399b9841c
Avaer Kazmer <a@modules.io>
2021-09-09 00:39:13 -0400

    Rewrite index.js to use new API

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import * as THREE from 'three';
-import {scene, renderer, camera, runtime, world, physics, ui, app, appManager} from 'app';
+// import {scene, renderer, camera, runtime, world, physics, ui, app, appManager} from 'app';
+import metaversefile from 'metaversefile';
+const {useFrame} = metaversefile;
 
-let updateIndex = 0;
-const physicsCube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshPhongMaterial({
-  color: 0xFF0000,
-}));
-scene.add(physicsCube);
-const physicsCubePhysicsId = physics.addBoxGeometry(new THREE.Vector3(0, 5, 0), new THREE.Quaternion(), new THREE.Vector3(0.5, 0.5, 0.5), true);
+export default () => {
+  const physicsCube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshPhongMaterial({
+    color: 0xFF0000,
+  }));
+  const physicsCubePhysicsId = physics.addBoxGeometry(new THREE.Vector3(0, 5, 0), new THREE.Quaternion(), new THREE.Vector3(0.5, 0.5, 0.5), true);
 
-renderer.setAnimationLoop((timestamp, frame) => {
-  if ((updateIndex % 100) === 0) {
-    physics.setPhysicsTransform(physicsCubePhysicsId, new THREE.Vector3(0, 10, 0), new THREE.Quaternion(0, 0, 0, 1));
-  }
-  const {position, quaternion} = physics.getPhysicsTransform(physicsCubePhysicsId);
-  physicsCube.position.copy(position);
-  physicsCube.quaternion.copy(quaternion);
-  updateIndex++;
-});
+  let updateIndex = 0;
+  useFrame(({timestamp}) => {
+    if ((updateIndex % 100) === 0) {
+      physics.setPhysicsTransform(physicsCubePhysicsId, new THREE.Vector3(0, 10, 0), new THREE.Quaternion(0, 0, 0, 1));
+    }
+    const {position, quaternion} = physics.getPhysicsTransform(physicsCubePhysicsId);
+    physicsCube.position.copy(position);
+    physicsCube.quaternion.copy(quaternion);
+    updateIndex++;
+  });
+  
+  return physicsCube;
+};
